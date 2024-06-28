cdad4c8a576d3bde91f12316c9588eaf151e6763
Mishael Enyi <mishaelcodes@outlook.com>
2024-06-28 18:04:13 +0100

    checked active tates for nav links and added a loading text

--- src/app/components/nav.jsx
+++ src/app/components/nav.jsx
@@ -1,5 +1,9 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 const Nav = () => {
+  const pathname = usePathname();
   return (
     <nav className="flex items-center justify-between max-w-screen py-5 px-20">
       <h1>
@@ -7,22 +11,42 @@ const Nav = () => {
       </h1>
       <ul className="flex items-center justify-evenly w-1/2">
         <li className="hover:scale-125 transition-all">
-          <Link href="/pricing" className="hover:font-semibold">
+          <Link
+            href="/pricing"
+            className={`hover:font-semibold ${
+              pathname === "/pricing" ? "font-bold text-green-600" : ""
+            }`}
+          >
             Pricing
           </Link>
         </li>
         <li className="hover:scale-125 transition-all">
-          <Link href="/services" className="hover:font-semibold">
+          <Link
+            href="/services"
+            className={`hover:font-semibold ${
+              pathname === "/services" ? "font-bold text-yellow-600" : ""
+            }`}
+          >
             Services
           </Link>
         </li>
         <li className="hover:scale-125 transition-all">
-          <Link href="/about" className="hover:font-semibold">
+          <Link
+            href="/about"
+            className={`hover:font-semibold ${
+              pathname === "/about" ? "font-bold text-red-600" : ""
+            }`}
+          >
             Who we are
           </Link>
         </li>
         <li className="hover:scale-125 transition-all">
-          <Link href="/contact" className="hover:font-semibold">
+          <Link
+            href="/contact"
+            className={`hover:font-semibold ${
+              pathname === "/contact" ? "font-bold text-blue-600" : ""
+            }`}
+          >
             Contact Us
           </Link>
         </li>
---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="w-screen min-h-screen flex items-center justify-center">
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default Loading;
